e1bdc1735e13ff5f19c28439f23c11a2006f0846
Vidya Sagar Singh <vidyasagarsingh000@gmail.com>
2020-04-12 22:15:45 +0530

    added push notification

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
-const express = require('express');
-const webPush = require('web-push');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const webPush = require("web-push");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname,'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.use(bodyParser.json());
+app.use(cors());
 
-const publicVapidKeys = 'BN5FobRashUcNaTXxIfVmYlBuI79vEfPflFXn9Ond48Tg1kMp0TJf1bdXZB1Ef6-LO6N70S_aTMEqTXu6U1iB3g';
-const privateVapidKeys = '0NyZCtnbpb4JJVLscrp5FbIlS0RL3holJF2V-cOqE3w';
+const publicVapidKeys =
+  "BN5FobRashUcNaTXxIfVmYlBuI79vEfPflFXn9Ond48Tg1kMp0TJf1bdXZB1Ef6-LO6N70S_aTMEqTXu6U1iB3g";
+const privateVapidKeys = "0NyZCtnbpb4JJVLscrp5FbIlS0RL3holJF2V-cOqE3w";
 
-webPush.setVapidDetails("mailto:test@terst.com",publicVapidKeys,privateVapidKeys);
+webPush.setVapidDetails(
+  "mailto:test@terst.com",
+  publicVapidKeys,
+  privateVapidKeys
+);
 
 //subscribe route
-app.post('/subscribe',(req,res)=>{
-    //get push subscription object
-    const subscription = req.body;
+app.post("/subscribe", (req, res) => {
+  //get push subscription object
+  const subscription = req.body;
 
-    //send status 201 resource created
-    res.status(201).json({});
+  //send status 201 resource created
+  res.status(201).json({});
 
-    //create payload
-    const payload = JSON.stringify({title:"push-test"});
+  //create payload
+  const payload = JSON.stringify({ title: "push-test" });
 
-    //pass object into send Notification
-    webPush.sendNotification(subscription,payload).catch(err=>console.error(err));
-
-})
+  //pass object into send Notification
+  webPush
+    .sendNotification(subscription, payload)
+    .catch((err) => console.error(err));
+});
 
 const port = 8080;
 
-app.listen(port,()=>console.log(`server listening at ${port}`))
+app.listen(port, () => console.log(`server listening at ${port}`));
--- src/src-sw.js
+++ src/src-sw.js
@@ -1,22 +1,25 @@
-console.log('serviceWorker loaded....');
+console.log("serviceWorker loaded....");
 
 /* eslint-disable no-restricted-globals */
 
 /* eslint-disable no-undef */
-    self.addEventListener("push", (e) => {
-        const data = e.data.json();
-        console.log(e.data)
-        console.log("push recieved...");
-        self.registration.showNotification(data.title, {
-          body: "notified by vidya sagar",
-          icon:
-            "https://cdn.pixabay.com/photo/2016/01/03/11/24/gear-1119298__340.png",
-        });
-      });
+self.addEventListener("push", (e) => {
+  const data = e.data.json();
+  console.log(e.data);
+  console.log("push recieved...");
+  self.registration.showNotification(data.title, {
+    body: "Get your Favorite grocery at Discounted Rates!!",
+    icon:
+      "https://cdn.pixabay.com/photo/2012/04/13/11/06/shield-31869_960_720.png",
+    badge:
+      "https://cdn.pixabay.com/photo/2016/01/03/11/24/gear-1119298__340.png",
+    image:
+      "https://cdn.pixabay.com/photo/2015/09/02/12/25/basket-918416_960_720.jpg",
+  });
+});
 
-      
- workbox.core.skipWaiting();
- workbox.core.clientsClaim();
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
@@ -25,26 +28,24 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest);
 //   {url: '/public/index.html', revision: null},
 //   {url: '/public/logo192.png', revision: null},
 //   {url: '/scripts/start.js', revision: null},
-  
+
 // ]);
 
-
 workbox.precaching.precacheAndRoute([
-    { url:'/src/logo.svg',revision:null},
-    {url:'/scripts/test.js',revision:null}
+  { url: "/src/logo.svg", revision: null },
+  { url: "/scripts/test.js", revision: null },
 ]);
 
 workbox.routing.registerRoute(
-    new RegExp('https://api.exchangeratesapi.io/latest'),
-    new workbox.strategies.CacheFirst({
-        cacheName:"currencies",
-        plugins:[
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 10*60
-            }),
-            
-        ]
-    })
-)
+  new RegExp("https://api.exchangeratesapi.io/latest"),
+  new workbox.strategies.CacheFirst({
+    cacheName: "currencies",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 10 * 60,
+      }),
+    ],
+  })
+);
 
-workbox.precaching.cleanupOutdatedCaches()
+workbox.precaching.cleanupOutdatedCaches();
